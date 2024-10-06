88f9f2851453303f92a65ca6a8f301d9b5536333
Tkachenko Pavel <Tkachenko.PI@dns-shop.ru>
2024-10-06 10:56:26 +1000

    Накидал структуру проекта

---
+++ Core/SaleItems/SaleItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.SaleItems
+{
+    public class SaleItem
+    {
+        string name { get; set; }
+        int id { get; set; }
+        float price { get; set; }
+
+    }
+}
--- EShop/Program.cs
+++ EShop/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SayWalcome();
+
+            while (true)
+            {
+
+                Console.WriteLine();
+
+            }
+        }
+
+        private static void SayWalcome()
+        {
+            Console.WriteLine("Добро пожаловать");
         }
     }
 }
