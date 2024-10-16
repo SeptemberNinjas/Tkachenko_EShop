0ffe802f4da6458bc36ecddb303f21f32e1408cb
Tkachenko Pavel <Tkachenko.PI@dns-shop.ru>
2024-10-16 16:55:35 +1000

    add PaymentTypes

---
+++ eshop.Core/PaymentTypes.cs
@@ -0,0 +1,17 @@
+﻿namespace eshop.Core;
+
+/// <summary>
+/// Типы оплаты
+/// </summary>
+public enum ItemTypes
+{
+    /// <summary>
+    /// Наличка
+    /// </summary>
+    Cash,
+
+    /// <summary>
+    /// Банковская карта
+    /// </summary>
+    BankCard
+}
